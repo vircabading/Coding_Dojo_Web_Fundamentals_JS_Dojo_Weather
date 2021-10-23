40f7c69f30059a338b33e3760404fcaaf6cf9fd6
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-10-22 21:18:41 -0700

    add friday faenheit and celsius temps

--- script.js
+++ script.js
@@ -20,13 +20,15 @@ function updateTemperature (element)    {               // Updates the temperatu
         document.getElementById("today-low-temp").innerText = "65°F";
         document.getElementById("tomorrow-high-temp").innerText = "80°F";
         document.getElementById("tomorrow-low-temp").innerText = "66°F";
-        document.getElementById("friday-hight-temp").innerText = "69°F";
+        document.getElementById("friday-high-temp").innerText = "69°F";
         document.getElementById("friday-low-temp").innerText = "61°F";
     } else {
         document.getElementById("today-high-temp").innerText = "24°C";
         document.getElementById("today-low-temp").innerText = "18°C";
         document.getElementById("tomorrow-high-temp").innerText = "27°C";
         document.getElementById("tomorrow-low-temp").innerText = "19°C";
-        
+        document.getElementById("friday-high-temp").innerText = "21°C";
+        document.getElementById("friday-low-temp").innerText = "16°C";
+
     }
 }

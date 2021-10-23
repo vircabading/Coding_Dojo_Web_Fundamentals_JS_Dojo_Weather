1c1b3ae730be74d0a8988e3533caa2c11cab86e7
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-10-22 21:16:04 -0700

    add friday farenheit temps

--- script.js
+++ script.js
@@ -16,16 +16,17 @@ function removeCookieBar () {                           // Removes the cookie ba
 
 function updateTemperature (element)    {               // Updates the temperature to Farenheit or Celsius
     if (element.value == "°F") {
-        console.log("Farenheit");
         document.getElementById("today-high-temp").innerText = "75°F";
         document.getElementById("today-low-temp").innerText = "65°F";
         document.getElementById("tomorrow-high-temp").innerText = "80°F";
         document.getElementById("tomorrow-low-temp").innerText = "66°F";
+        document.getElementById("friday-hight-temp").innerText = "69°F";
+        document.getElementById("friday-low-temp").innerText = "61°F";
     } else {
-        console.log("Celsius");
         document.getElementById("today-high-temp").innerText = "24°C";
         document.getElementById("today-low-temp").innerText = "18°C";
         document.getElementById("tomorrow-high-temp").innerText = "27°C";
         document.getElementById("tomorrow-low-temp").innerText = "19°C";
+        
     }
 }

fc76b8018ba398e62b4778d5c6243d05d0d09efb
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-10-22 21:03:19 -0700

    add function to detect if farenheit or celsius is wanted temp

--- script.js
+++ script.js
@@ -10,6 +10,16 @@
 //////////////////////////////////////////////////
 // FUNCTIONS
 
-function removeCookieBar () {
+function removeCookieBar () {                           // Removes the cookie bar
     document.getElementById("cookie-bar"). remove();
+}
+
+function updateTemperature (element)    {               // Updates the temperature to Farenheit or Celsius
+    if (element.value == "°F") {
+        console.log("Farenheit");
+        document.getElementById("today-high-temp").innerText = "75°F";
+    } else {
+        console.log("Celsius");
+        document.getElementById("today-high-temp").innerText = "24°C"
+    }
 }

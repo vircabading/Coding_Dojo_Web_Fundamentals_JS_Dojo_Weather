31debca45587b104d5f93db53a71530e2029c8af
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-10-22 21:11:38 -0700

    add today and tomorrow temp change

--- script.js
+++ script.js
@@ -18,8 +18,14 @@ function updateTemperature (element)    {               // Updates the temperatu
     if (element.value == "°F") {
         console.log("Farenheit");
         document.getElementById("today-high-temp").innerText = "75°F";
+        document.getElementById("today-low-temp").innerText = "65°F";
+        document.getElementById("tomorrow-high-temp").innerText = "80°F";
+        document.getElementById("tomorrow-low-temp").innerText = "66°F";
     } else {
         console.log("Celsius");
-        document.getElementById("today-high-temp").innerText = "24°C"
+        document.getElementById("today-high-temp").innerText = "24°C";
+        document.getElementById("today-low-temp").innerText = "18°C";
+        document.getElementById("tomorrow-high-temp").innerText = "27°C";
+        document.getElementById("tomorrow-low-temp").innerText = "19°C";
     }
 }
